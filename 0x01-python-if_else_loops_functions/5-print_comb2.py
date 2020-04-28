4cc6d7595e026b795f78b4e2c5d653e9e9e7cb9e
sarias12 <1638@holbertonschool.com>
2020-04-28 20:07:16 +0000

    Fix Task-5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for num in range(0, 99):
-    print("{}".format(num), end=", ")
+for num in range(00, 99):
+    num1 = num // 10
+    num2 = num % 10
+    print("{}{}".format(num1, num2), end=", ")
 print("{}".format(99))
