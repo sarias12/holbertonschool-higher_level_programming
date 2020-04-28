873f5a9a319737fb6b1816ed3c09deb95050111a
sarias12 <1638@holbertonschool.com>
2020-04-28 19:15:00 +0000

    Fix task-2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for character in range(ord('a'), ord('z') + 1):
-    print(chr(character), end="")
+for character in range(97, 123):
+    print("{}".format(chr(character)), end="")
