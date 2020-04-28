6ebf0147eb0ca4fe46ba27bcfb8493d192b4eba7
sarias12 <1638@holbertonschool.com>
2020-04-28 18:02:32 +0000

    task-0 and Readme File

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number > 0:
+    print("{:d} is positive".format(number))
+elif number < 0:
+    print("{:d} is negative".format(number))
+else:
+    print("{:d} is zero".format(number))
