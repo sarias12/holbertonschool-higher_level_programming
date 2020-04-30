6670ce1e470d5c5e35db102b071da80ddd8ed5a5
sarias12 <1638@holbertonschool.com>
2020-04-30 22:27:05 +0000

    Task-2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    args = len(argv)
+    if args == 1:
+        print("{} arguments".format(0))
+    elif args == 2:
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
+    else:
+        print("{} arguments:".format(args - 1))
+        for index in range(1, args):
+            print("{}: {}".format(index, argv[index]))
