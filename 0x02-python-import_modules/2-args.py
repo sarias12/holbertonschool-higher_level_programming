a02116639647f86ca30cdb2ca2073ba1d025a961
sarias12 <1638@holbertonschool.com>
2020-04-30 22:45:51 +0000

    Fix Task-2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from sys import argv
     args = len(argv)
     if args == 1:
-        print("{} arguments".format(0))
+        print("{} arguments.".format(0))
     elif args == 2:
         print("1 argument:")
     else:
