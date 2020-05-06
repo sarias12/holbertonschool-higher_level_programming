aff535f8bcbcab47cdc7421cf38ab746bfe420e9
sarias12 <1638@holbertonschool.com>
2020-05-06 02:01:10 +0000

    Fix Task 11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if <= 0:
+    if len(my_list) <= idx:
         return my_list
     my_list = my_list[:idx] + my_list[idx + 1:]
     return my_list
