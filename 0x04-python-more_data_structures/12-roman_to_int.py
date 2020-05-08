3185b889956e9cd85578bc9f02e563211ac11a7c
sarias12 <1638@holbertonschool.com>
2020-05-08 02:11:48 +0000

    Task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,12 +2,10 @@
 def roman_to_int(roman_string):
     number = 0
     arabic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    if roman_string == 'IV':
-        return 4
-    if roman_string == 'IX':
-        return 9
-    if not roman_string:
+    if not roman_string or type(roman_string) != str:
         return None
     for letter in roman_string:
         number += arabic.get(letter)
+    if roman_string[-2:] == 'IV' or roman_string[-2:] == 'IX':
+        number -= 2
     return number
