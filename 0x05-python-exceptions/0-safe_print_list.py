1ff72cf85f0f35fb6b3fff0b0f7dc87e232a1843
Sergio Steben Arias Quintero <1638@holbertonschool.com>
2020-05-18 19:51:57 +0000

    Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,10 @@
 #!/usr/bin/pyhton3
 def safe_print_list(my_list=[], x=0):
     try:
-        count = 0
-        for element in my_list[:x]:
-            count += 1
-            print("{}".format(element), end="")
+        for count in range(x):
+            print("{}".format(my_list[count]), end="")
         print("")
-    except IndexError:
+    except:
         print("")
-    return count
+        return count
+    return count + 1
