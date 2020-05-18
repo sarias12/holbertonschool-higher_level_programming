3ffae4765cac2023a357e24ab42d263d1f173274
Sergio Steben Arias Quintero <1638@holbertonschool.com>
2020-05-18 22:30:27 +0000

    Task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    i = 0
+    for elements in range(0, x):
+        try:
+            print("{:d}".format(my_list[elements]), end="")
+        except (TypeError, ValueError):
+            continue
+        except IndexError:
+            break
+        i += 1
+    print("")
+    return i
