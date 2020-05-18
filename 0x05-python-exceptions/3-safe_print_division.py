0d199b8e57afbf5778189529c6f5824573c5c9e0
Sergio Steben Arias Quintero <1638@holbertonschool.com>
2020-05-18 23:13:38 +0000

    Task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,9 +2,11 @@
 def safe_print_division(a, b):
     result = 0
     try:
-        result = a / b
-        return result
+        result = a/b
+       
     except (ZeroDivisionError, ValueError, TypeError):
-        return None
+        result = None
+    
     finally:
         print("Inside result: {}".format(result))
+    return result
