1c67b90f1205efcf41224d94aa1b840f086545b8
Sergio Steben Arias Quintero <1638@holbertonschool.com>
2020-05-19 22:59:46 +0000

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Class that defines a square. """
+
+
+class Square:
+    def __init__(self, size):
+        """Defining attributes for instance  - Square Class.
+
+        Args:
+            size (int): Parameter indicating the size of the square.
+
+        Attributes:
+            size (int): size of the square.
+
+        Returns:
+            Nothing.
+        """
+        self.__size = size
