54b8049994f4001a214c53431ad40de153f700a3
Sergio Steben Arias Quintero <1638@holbertonschool.com>
2020-05-19 00:11:20 +0000

    Task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -11,7 +11,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             print("division by 0")
         except IndexError:
-            print("division by 0")
+            print("out of range")
         finally:
             new_list.append(result)
     return new_list
