b3a2e6ba5bf3a8b6defe782b5227d3457e482905
Sergio Steben Arias Quintero <1638@holbertonschool.com>
2020-06-09 21:54:13 +0000

    Fix pep8

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,38 +1,41 @@
 #!/usr/bin/python3
 """ Base Module
 """
+
+
 import json
+
 
 class Base:
     """
     Base Superclass of Rectangle class and Square Class.
     """
     __nb_objects = 0
+
     def __init__(self, id=None):
         """
         __init__ method for inizilizating
 
         Args:
-            id (int): number to identify instantiated objects. Defaults to None.
+            id (int): number to identify instantiated objects.
+            Defaults to None.
         """
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
-    
+
     def to_json_string(list_dictionaries):
+        """to_json_string [summary]
+
+        Args:
+            list_dictionaries ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
-
-    @classmethod
-    def save_to_file(cls, list_objs):
-        ficher = open("{}.json".format(cls.__name__), mode='w')
-
-
-
-
-
-        
