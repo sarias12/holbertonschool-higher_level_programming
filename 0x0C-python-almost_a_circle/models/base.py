9b79ee1e5904d8f92f9703ccf1d5f40d54508c2d
Sergio Steben Arias Quintero <1638@holbertonschool.com>
2020-06-10 01:18:59 +0000

    Task 16 and task 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -40,3 +40,32 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+    
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """save_to_file [
+
+        Args:
+            list_objs (class): is a list of instances who inherits of Base.
+        """
+        new_list = []
+        with open("{}.json".format(cls.__name__), 'w') as file_1:
+            if list_objs is not None:
+                for item in list_objs:
+                    new_list.append(cls.to_dictionary(item))
+            file_1.write(cls.to_json_string(new_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """from_json_string [summary]
+
+        Args:
+            json_string: string representing a list of dictionaries
+
+        Returns:
+            [str]: the list represented by json_string
+        """
+        if json_string is None:
+            return []
+        else:
+            return json.loads(json_string)
