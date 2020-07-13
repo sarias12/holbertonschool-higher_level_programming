785cfe7fc4f7b6cf33ee06726252cab84e4ae22c
sarias12 <1638@holbertonschool.com>
2020-07-13 20:53:21 +0000

    FIX Tasks 1 AND 2

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 -- This script creates the database hbtn_0c_0
 -- Taks 1 - Create a database
-CREATE DATABASE hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
 -- This script deletes hbtn_0c_0 database.
 -- Task 2 - Delete a database
-DROP DATABASE hbtn_0c_0;
+DROP DATABASE IF EXISTS hbtn_0c_0;
