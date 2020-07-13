877e476baba9dd9ae3f76ecfeb078c6e7a0f46ed
sarias12 <1638@holbertonschool.com>
2020-07-13 22:45:04 +0000

    Task 11 - all records with a score >= 10

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- This script lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0.
+-- Task 11 - Select all records according to score.
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
