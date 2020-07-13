5a1eeb7e2ca08dbffd5e15d6c7f8ce1188f6090a
sarias12 <1638@holbertonschool.com>
2020-07-13 22:31:42 +0000

    Task 10 - Lists all rows second table

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- This script lists all rows of the table second_table from the database hbtn_0c_0.
 -- Task 10 - List all in the table - second Table.
-SELECT * FROM second_table;
+SELECT score, name FROM second_table ORDER BY score DESC;
