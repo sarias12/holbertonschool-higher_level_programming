a86b0d9174b6ad51bef052fa8da38b43473d07e3
sarias12 <1638@holbertonschool.com>
2020-07-13 23:18:06 +0000

    Task 14 - average of score

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
 -- This script computes the score average of all records in the table second_table of the database hbtn_0c_0.
 -- Task 14 - Average.
-SELECT AVG(score) FROM `second_table`;
+SELECT AVG(score) AS average FROM `second_table`;
