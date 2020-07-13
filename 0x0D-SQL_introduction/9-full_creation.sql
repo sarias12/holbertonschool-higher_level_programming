db9e164b17075dc4db30da745f42d99c460c5d63
sarias12 <1638@holbertonschool.com>
2020-07-13 22:16:23 +0000

    Task 9 - Full creation table with records

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+-- This scrip creates second table with records.
+-- Task 9 - Full creation of table with records.
+CREATE TABLE second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
