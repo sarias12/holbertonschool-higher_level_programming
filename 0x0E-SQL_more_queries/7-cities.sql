a406626c41f05c219c5f8fc82d04b343a9c17e7d
sarias12 <1638@holbertonschool.com>
2020-07-14 21:24:45 +0000

    Task 7 - Creates database and table with foreing key.

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,5 @@
+-- This script creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa).
+-- Task 7 - Creates database and table.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (id INT AUTO_INCREMENT UNIQUE NOT NULL, name VARCHAR(256) NOT NULL, state_id INT NOT NULL, PRIMARY KEY (id), FOREIGN KEY (state_id) REFERENCES states(id));
