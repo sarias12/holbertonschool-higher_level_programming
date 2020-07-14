553c1e9d0e4bfb9458a94ddc916d2956d4f188bb
sarias12 <1638@holbertonschool.com>
2020-07-14 00:24:11 +0000

    Task 16 display all names

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- This script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
 -- Task 16 - displas all names of second table
-SELECT score, name FROM second_table WHERE name IS NOT NULL;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
