d6d80139d456b7a143b0918c94afdba801a4f3c6
sarias12 <1638@holbertonschool.com>
2020-07-14 19:57:49 +0000

    Task 3 - creates table with mandatory name

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,3 @@
+-- This script creates the table force_name on your MySQL server.
+-- Task 3 - creates table with mandatory name.
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
