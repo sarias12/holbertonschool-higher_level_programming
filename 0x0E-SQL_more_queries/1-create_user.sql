e9038a1418fbd6b8272a079f55fec901c9229a7d
sarias12 <1638@holbertonschool.com>
2020-07-14 19:32:16 +0000

    Task 1 -root user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- This script  creates the MySQL server user user_0d_1.
+-- Task 1 - root user.
+CREATE USER IF NOT EXISTS 'user_01_1'@'localhost';
+SET PASSWORD FOR 'user_01_1'@'localhost' = 'user_0d_1_pwd';
