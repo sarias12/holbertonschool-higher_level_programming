1e549b78ec401dbe7cc64c351b83d21ec24aeafc
sarias12 <1638@holbertonschool.com>
2020-07-15 00:13:36 +0000

    Task 10 - display tv_show with genre id

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,3 @@
+-- This script  lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+-- Task 10 - display tv_show with genre id
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres WHERE tv_shows.id = tv_show_genres.show_id ORDER BY title, genre_id ASC;
