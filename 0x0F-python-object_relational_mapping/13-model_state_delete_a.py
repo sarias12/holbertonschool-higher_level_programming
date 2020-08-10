31d3c4cc2bc45cfdf6dfb80f5d05f22aceabb566
sarias12 <1638@holbertonschool.com>
2020-08-10 02:01:41 +0000

    Task 13

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""lists all State objects that contain the letter a
-from the database hbtn_0e_6_usa
+"""deletes all State objects with a name containing the letter a
+from the database hbtn_0e_6_usa.
 """
 import sys
 from model_state import Base, State
