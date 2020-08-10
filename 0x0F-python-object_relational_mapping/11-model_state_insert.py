094fd373dc9f04e7fde7f54e9ef1837c04413850
sarias12 <1638@holbertonschool.com>
2020-08-10 00:23:14 +0000

    Task 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""lists all State objects that contain the letter a
+from the database hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]
+            ),
+        pool_pre_ping=True
+        )
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new_state = State(name='Louisiana')
+    session.add(new_state)
+    result = session.query(State).\
+        filter(State.name.like('Louisiana')).first()
+
+    if result:
+        print("{}".format(result.id))
+    else:
+        print("Not found")
