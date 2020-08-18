b4b62f76807315dd9f1ebf77fd7dd7e9f82a02e1
sarias12 <1638@holbertonschool.com>
2020-08-18 21:10:31 +0000

    Task 5 - POST request to the passed URL

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#This script sends a POST request to the passed URL, and displays the body of the response
+#This script sends a POST request to the passed URL, and displays the body of the response.
 curl -sd "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
