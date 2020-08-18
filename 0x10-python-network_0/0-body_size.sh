f44e507fccb50b886c3cd6380981da937edc6867
sarias12 <1638@holbertonschool.com>
2020-08-18 18:51:30 +0000

    Task 0 - Displays size of the body of the response of a request url.

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #this script displays the size of the body of the response of a request url.
-curl 0.0.0.0:5000 -sI | grep "Length" | cut -d " " -f2
+curl "$1" -sI | grep "Length" | cut -d " " -f2
