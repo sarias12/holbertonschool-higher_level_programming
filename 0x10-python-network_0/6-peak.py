1ebe22851a0b511f3dcad7c1ace9ab7e113a68f7
sarias12 <1638@holbertonschool.com>
2020-08-19 01:57:25 +0000

    Task 6 - peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,16 +4,6 @@
 
 def find_peak(list_of_integers):
     """"Check if it's a peak"""
-    if len(list_of_integers) == 0:
-        return None
-    lt = list_of_integers
-    start = 1
-    end = len(lt) - 1
-    peak = lt[0]
-    while start < end:
-        if lt[start] > lt[start - 1] and lt[start] > lt[start + 1]:
-            peak = lt[start]
-            start += 1
-        else:
-            start += 1
-    return peak
+    if list_of_integers:
+        list_of_integers.sort()
+        return list_of_integers[-1]
