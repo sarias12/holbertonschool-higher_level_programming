414cd730f1f95c3a556403608a6f2bdb831305e8
sarias12 <1638@holbertonschool.com>
2020-08-25 00:38:28 +0000

    Task - 9 Github API to display your id

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Python script that takes your Github credentials
-(username and password) and uses the Github API to display your id
+(username and password) and uses the Github API to display your id.
 """
 import requests
 import sys
