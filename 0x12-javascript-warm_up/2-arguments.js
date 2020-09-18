88fdae2474a97a3e6c96b57221a0f2bdd6ccab0a
sarias12 <1638@holbertonschool.com>
2020-09-18 03:21:17 +0000

    Task 2 - Script that prints a message depending of the number of arguments passed.

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/*
+Script that prints a message depending of the number of arguments passed.
+*/
+const args = process.argv.length;
+if (args === 2) {
+  console.log('No argument');
+} else if (args === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
