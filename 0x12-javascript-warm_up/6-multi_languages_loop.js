3a375b9fe3c6bbf3b6cc7ff619b0e083622c6374
sarias12 <1638@holbertonschool.com>
2020-09-18 04:20:04 +0000

    Task 6 - script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/*
+script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
+*/
+const list = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const int = list.length;
+for (let i = 0; i < int; i++) {
+  console.log(list[i]);
+}
