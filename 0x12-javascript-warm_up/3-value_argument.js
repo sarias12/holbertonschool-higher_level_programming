77636cf7e56417551229f5b73653f29f2123e8ca
sarias12 <1638@holbertonschool.com>
2020-09-18 03:50:04 +0000

    Task 3 - Script that prints the first argument passed to it

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,7 +2,7 @@
 /*
 Script that prints the first argument passed to it.
 */
-if (process.argv.length === 2) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
 } else {
   const argvs = process.argv.slice(2);
