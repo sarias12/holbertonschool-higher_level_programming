fa2c52748ee28ed470b8548e14abb574769b9190
Steben Arias <1638@holbertonschool.com>
2020-09-21 17:46:00 -0500

    Task 1 - script that defines a rectangle with width and height attributes

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 /* script that defines a rectangle with width and height attributes */
 class Rectangle {
-  constructor (h, w) {
-    this.height = h;
+  constructor (w, h) {
     this.width = w;
+    this.height = h;
   }
 }
 module.exports = Rectangle;
