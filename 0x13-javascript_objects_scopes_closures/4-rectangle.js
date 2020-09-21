79c5d1b5334d132788fd00c30bad33285c4a414c
Steben Arias <1638@holbertonschool.com>
2020-09-21 18:26:32 -0500

    Task 1 - script that defines a rectangle with width and height attributes. Adding  print, double and  rotate methods

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+/*
+Script that defines a rectangle with width and height attributes,
+if w or h is less than  or equal 0 create a empty object and
+Create an instance method called print() that prints the rectangle using the character X
+*/
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let str = '';
+      for (let j = 0; j < this.width; j++) {
+        str += 'X';
+      }
+      console.log(str);
+    }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
