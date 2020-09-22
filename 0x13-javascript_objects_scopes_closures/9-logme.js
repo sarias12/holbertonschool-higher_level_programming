79847e0638d5924e4f8a72db8b72c612fa2680f9
Steben Arias <1638@holbertonschool.com>
2020-09-21 20:29:15 -0500

    Task 9 - Script that converts a number from base 10 to another base passed as argument.

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/*
+Script that converts a number from base 10 to another base passed as argument.
+*/
+let number = 0;
+exports.logMe = function (item) {
+  console.log(number + ': ' + item);
+  number += 1;
+};
