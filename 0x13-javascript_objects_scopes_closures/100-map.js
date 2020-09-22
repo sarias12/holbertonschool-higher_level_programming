e7b63bf77d8934f457175401817538d60727a7fa
Steben Arias <1638@holbertonschool.com>
2020-09-21 21:47:26 -0500

    Task 11 - Script  that imports an array and computes a new array.

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -3,6 +3,11 @@
 Script  that imports an array and computes a new array.
 */
 const data = require('./100-data');
-const map1 = data.list.map(x => x * data.list.indexOf(x));
+let num1 = 0;
+const map1 = data.list.map(function (num) {
+  const num2 = num * num1;
+  num1 += 1;
+  return (num2);
+});
 console.log(data.list);
 console.log(map1);
