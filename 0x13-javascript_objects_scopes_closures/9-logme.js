b029b99a728e330e1b351c9b730a1d597d1a2185
Steben Arias <1638@holbertonschool.com>
2020-09-21 20:30:43 -0500

    Task 9 - Script that prints the number of arguments already printed and the new argument value.

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /*
-Script that converts a number from base 10 to another base passed as argument.
+Script that prints the number of arguments already printed and the new argument value.
 */
 let number = 0;
 exports.logMe = function (item) {
