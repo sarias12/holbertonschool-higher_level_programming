cd40f0c6183454284d6fe930d3538c2800cc37d5
Steben Arias <1638@holbertonschool.com>
2020-09-22 23:38:18 -0500

    Task 2 - Script that display the status code of a GET request.

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*
+Script that display the status code of a GET request.
+*/
+const request = require('request');
+const url = process.argv[2];
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
+});
