66a568cb9693cadb39dc9282fffe76fb360537ba
Steben Arias <1638@holbertonschool.com>
2020-09-24 22:20:07 -0500

    script that adds the class red to the HTML tag HEADER when the user clicks on the tag DIV#red_header

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/*
+script that adds the class red to the HTML tag HEADER when the user clicks on the tag DIV#red_header
+*/
+$('DIV#red_header').click(function () {
+  $('header').addClass('red');
+});
