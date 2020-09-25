8be256039f2fe679759e34603d56248300a48e80
Steben Arias <1638@holbertonschool.com>
2020-09-24 23:44:11 -0500

    Task 9 -  script that fetches from https://fourtonfish.com/hellosalut/?lang=fr and displays the value of hello from that fetch in the HTML’s tag DIV#hello

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/*
+ script that fetches from https://fourtonfish.com/hellosalut/?lang=fr and displays the value of hello from that fetch in the HTML’s tag DIV#hello
+*/
+$.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
+  $('DIV#hello').text(data['hello']);
+});
